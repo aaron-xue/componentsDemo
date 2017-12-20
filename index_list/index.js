ad5e6e055c157f1e5850078cd8f4bbf712b79480
xuechunhui <xch1523480@163.com>
2017-12-20 18:37:55 +0800

    修改

--- index_list/index.js
+++ index_list/index.js
@@ -1,1 +1,41 @@
-$(function(){var e=$(".content"),n=$("#menu");$.each(cityData,function(a,t){var r=$('<li class="item" id="item'+a+'"></li>');r.append('<h2 class="item_letter">'+t.name+"</h2>"),n.append('<span class="letter" name="#item'+a+'">'+t.name+"</span>"),n.find("span:first-of-type").addClass("current");var i=$("<ul></ul>");$.each(t.cities,function(e,n){i.append("<li>"+n.name+"</li>")}),r.append(i),e.append(r)});var a=new BScroll(".wrapper",{probeType:3});a.on("scroll",function(e){var n=$(document).scrollTop(),a="";$("#content").find(".item").each(function(){var e=$(this),t=e.offset().top;n>t-200&&(a="#"+e.attr("id"))}),a!=$("#menu").find(".current").attr("name")&&($("#menu").find(".current").removeClass("current"),$("#menu").find("span[name='"+a+"']").addClass("current"))}),$(".l-cell").delegate("span","click",function(){var e=$(".l-cell span").index($(this));a.scrollToElement($(".brand-list .item").eq(e)[0],"300"),$(this).siblings().removeClass("current").end().addClass("current")})});
+$(function () {
+    var $content = $('.content');
+    var $menu = $('#menu');
+    $.each(cityData, function (index, item) {
+        var $itemGroup = $('<li class="item" id="item' + index + '"></li>')
+        $itemGroup.append('<h2 class="item_letter">' + item.name + '</h2>')
+        $menu.append('<span class="letter" name="#item' + index + '">' + item.name + '</span>')
+        $menu.find("span:first-of-type").addClass("current");
+        var $itemlist = $('<ul></ul>')
+        $.each(item.cities, function (ide, ite) {
+            $itemlist.append('<li>' + ite.name + '</li>')
+        })
+        $itemGroup.append($itemlist)
+        $content.append($itemGroup);
+    })
+    var scroll = new BScroll('.wrapper', {
+        probeType: 3
+    })
+    scroll.on('scroll', function (pos) {
+        var scrollTop = $(document).scrollTop();
+        var contentItems = $("#content").find(".item");
+        var currentItem = "";
+        contentItems.each(function () {
+            var contentItem = $(this);
+            var offsetTop = contentItem.offset().top;
+            if (scrollTop > offsetTop - 200) {
+                currentItem = "#" + contentItem.attr("id");
+            }
+        });
+        if (currentItem != $("#menu").find(".current").attr("name")) {
+            $("#menu").find(".current").removeClass("current");
+            $("#menu").find("span[name='" + currentItem + "']").addClass("current");
+        }
+    });
+    $(".l-cell").delegate("span", "click", function () {
+        var allHeight = 0;
+        var index = $(".l-cell span").index($(this));
+        scroll.scrollToElement( $(".brand-list .item").eq(index)[0], '300');
+        $(this).siblings().removeClass("current").end().addClass("current");
+    });
+})
